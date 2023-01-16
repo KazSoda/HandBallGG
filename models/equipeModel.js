0b0f09725c67458b31b5f082435041dd2d2f7d9e
graillet <theo.graille@etu.univ-grenoble-alpes.fr>
2023-01-16 17:24:56 +0100

    equipeModel finished

--- models/equipeModel.js
+++ models/equipeModel.js
@@ -4,15 +4,45 @@ const validator = require('validator');
 
 const equipeSchema = new mongoose.Schema({
 
-	
-	phone: {
-		type: String,
-		required: [true, 'Un equipe doit avoir un numéro de téléphone.'],
+	wording: {
+		type:String,
+		required: [true,'Une équipe doit avoir un libelle'],
 		trim: true,
 		unique: true,
-		maxLength: 10,
-		minLength: 10,
-		validate: [validator.isNumeric, 'Veuillez entrer un numéro de téléphone valide.'],
+		maxLength: 64,
+	},
+
+	trainer: {
+		type:String,
+		required:[true,'Une équipe doit avoir un entraineur'],
+		trim: true,
+		maxLength:128,
+	},
+
+	slot: {
+		type:String,
+		required: [true, 'Une équipe doit avoir un creneaux'],
+		trim: true,
+		maxLength:128,
+	},
+
+	urlPhoto: {
+		type:String,
+		required: [true, 'Une équipe doit avoir une photo'],
+		trim:true,
+		maxLength:512,
+	},
+
+	urlCalendar: {
+		type:String,
+		required: [true, 'Une équipe doit avoir un calendrier'],
+		trim:true,
+		maxLength:512,
+	},
+
+	comment: {
+		type:String,
+		trim: true,
 	}
 	
 })
