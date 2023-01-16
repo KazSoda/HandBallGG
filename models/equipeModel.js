92d8750a00ea05feb93c653536c8139697f63217
Yohann <73314529+yohann69@users.noreply.github.com>
2023-01-16 17:38:34 +0100

    Formatting

--- models/equipeModel.js
+++ models/equipeModel.js
@@ -1,50 +1,47 @@
 
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const equipeSchema = new mongoose.Schema({
 
-	wording: {
-		type:String,
-		required: [true,'Une équipe doit avoir un libelle'],
-		trim: true,
-		unique: true,
-		maxLength: 64,
-	},
+    wording: {
+        type: String,
+        required: [true, 'Une équipe doit avoir un libelle'],
+        trim: true,
+        unique: true,
+        maxLength: 64,
+    },
 
-	trainer: {
-		type:String,
-		required:[true,'Une équipe doit avoir un entraineur'],
-		trim: true,
-		maxLength:128,
-	},
+    trainer: {
+        type: String,
+        required: [true, 'Une équipe doit avoir un entraineur'],
+        trim: true,
+        maxLength: 128,
+    },
 
-	slot: {
-		type:String,
-		required: [true, 'Une équipe doit avoir un creneaux'],
-		trim: true,
-		maxLength:128,
-	},
+    slot: {
+        type: String,
+        required: [true, 'Une équipe doit avoir un creneaux'],
+        trim: true,
+        maxLength: 128,
+    },
 
-	urlPhoto: {
-		type:String,
-		required: [true, 'Une équipe doit avoir une photo'],
-		trim:true,
-		maxLength:512,
-	},
+    photo: {
+        type: String,
+        required: [true, 'Une équipe doit avoir une photo'],
+        trim: true,
+    },
 
-	urlCalendar: {
-		type:String,
-		required: [true, 'Une équipe doit avoir un calendrier'],
-		trim:true,
-		maxLength:512,
-	},
+    urlCalendar: {
+        type: String,
+        required: false,
+        trim: true,
+    },
 
-	comment: {
-		type:String,
-		trim: true,
-	}
-	
+    comment: {
+        type: String,
+        trim: true,
+    }
+
 })
 
 
