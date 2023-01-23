8129f7161f764f36d724d141b134c03ac3a06081
graillet <theo.graille@etu.univ-grenoble-alpes.fr>
2023-01-23 14:35:04 +0100

    equipeController finish

--- controllers/equipeController.js
+++ controllers/equipeController.js
@@ -1,3 +1,5 @@
+
+
 const Equipe = require('../models/equipeModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
@@ -29,4 +31,52 @@ exports.createEquipe = catchAsync(async(req,res) => {
             newEquipe
         }
     }) 
+})
+
+exports.delEquipe = catchAsync(async(req,res) => {
+    const delEquipe = await Equipe.findByIdAndDelete(req.params.id)
+
+    if (!delEquipe){
+        return next(new AppError('Aucune équipe trouvé à cette id',404))
+    }
+
+    res.status(204).json({
+        status:'success',
+        data:null
+    })
+})
+
+exports.UpdateEquipe = catchAsync(async(req,res) => {
+    const updateEquipe = await Equipe.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    })
+
+    if (!updateEquipe){
+        return next(new AppError('Aucune équipe trouvé à cette id',404))
+    }
+
+    res.status(200).json({
+        status:'success',
+        data:{
+            updateEquipe
+        }
+    })
+
+
+})
+
+exports.getOne = catchAsync(async(req,res) => {
+    const equipe = Equipe.findById(req.params.id);
+
+    if(!equipe){
+        return next(new AppError('Aucune équipe trouvé à cette id',404))
+    }
+
+    res.status(200).json({
+        status:'success',
+        data:{
+            equipe
+        }
+    })
 })
