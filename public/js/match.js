5d614aafbea84f9c8b2c86868de8f924e4906f60
Yohann <73314529+yohann69@users.noreply.github.com>
2023-03-03 14:58:04 +0100

    📨 Formatting the document

--- public/js/match.js
+++ public/js/match.js
@@ -2,54 +2,54 @@ import axios from "axios";
 import { showAlert } from "./alert";
 
 export const matchInformation = async () => {
-  let currentDate=new Date();
-  let currentDate2=new Date();
-  let threeWeekAgo = currentDate.addDays(- (7*3));
-  let twoMonthAfter = currentDate2.addDays(7*8);
-  try {
-    const res = await axios({
-      method: "get",
-      url: `/api/v1/match?sort=date&date[gt]=${threeWeekAgo}&date[lt]=${twoMonthAfter}`,
-    });
-    if (res.data.status === "success") {
-      let inputSearchBar = document.querySelector("#searchInformation");
-      searchMatchByTeam(res.data.data.matchs, inputSearchBar);
-    }
-  } catch (err) {
-    showAlert("error", err.response.data.message);
-  }
+	let currentDate = new Date();
+	let currentDate2 = new Date();
+	let threeWeekAgo = currentDate.addDays(- (7 * 3));
+	let twoMonthAfter = currentDate2.addDays(7 * 8);
+	try {
+		const res = await axios({
+			method: "get",
+			url: `/api/v1/match?sort=date&date[gt]=${threeWeekAgo}&date[lt]=${twoMonthAfter}`,
+		});
+		if (res.data.status === "success") {
+			let inputSearchBar = document.querySelector("#searchInformation");
+			searchMatchByTeam(res.data.data.matchs, inputSearchBar);
+		}
+	} catch (err) {
+		showAlert("error", err.response.data.message);
+	}
 };
 
 function searchMatchByTeam(queryResult, enteredValue) {
-  let mainSection = document.querySelector(".mainSection");
-  let resSort = [];
-  queryResult.forEach((matchInfo, index, matchInfoFull) => {
-    if (
-      matchInfo.againstTeam
-        .toUpperCase()
-        .includes(enteredValue.value.toUpperCase()) ||
-      matchInfo.localTeam
-        .toUpperCase()
-        .includes(enteredValue.value.toUpperCase())
-    ) {
-      resSort.push(matchInfo);
-    }
-  });
+	let mainSection = document.querySelector(".mainSection");
+	let resSort = [];
+	queryResult.forEach((matchInfo, index, matchInfoFull) => {
+		if (
+			matchInfo.againstTeam
+				.toUpperCase()
+				.includes(enteredValue.value.toUpperCase()) ||
+			matchInfo.localTeam
+				.toUpperCase()
+				.includes(enteredValue.value.toUpperCase())
+		) {
+			resSort.push(matchInfo);
+		}
+	});
 
 
 
-  if (resSort.length == 0) {
-    mainSection.innerHTML = `<h1>La recherche n'a pas donné de résultats</h1>`;
-  } else {
-    mainSection.innerHTML =''
-    resSort.forEach(sortedMatch => {
-      let date = new Date(sortedMatch.date).toDateString()
-
-      
+	if (resSort.length == 0) {
+		mainSection.innerHTML = `<h1>La recherche n'a pas donné de résultats</h1>`;
+	} else {
+		mainSection.innerHTML = ''
+		resSort.forEach(sortedMatch => {
+			let date = new Date(sortedMatch.date).toDateString()
 
 
 
-      mainSection.innerHTML += `
+
+
+			mainSection.innerHTML += `
 
 
       <section class="matchSection">
@@ -70,22 +70,22 @@ function searchMatchByTeam(queryResult, enteredValue) {
     </section>
 
     `;
-    });
-  }
+		});
+	}
 }
 
 
 document.querySelector("#searchMatchBySelect").addEventListener("change", (e) => {
-  select = document.getElementById("searchMatchBySelect");
-  choice = select.selectedIndex
-  valueSelected = select.options[choice].value;
-  console.log(valueSelected);
+	select = document.getElementById("searchMatchBySelect");
+	choice = select.selectedIndex
+	valueSelected = select.options[choice].value;
+	console.log(valueSelected);
 });
 
 
 
-Date.prototype.addDays = function(days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
+Date.prototype.addDays = function (days) {
+	var date = new Date(this.valueOf());
+	date.setDate(date.getDate() + days);
+	return date;
 }
