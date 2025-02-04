58d5b29d0f23917a3330c415fd83dcab4fcc3bd9
Sôda <alansapetind@gmail.com>
2025-02-04 12:23:48 +0100

    MAYBE

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = require('./app');
 
 const db = process.env.DATABASE_mongodb
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 
 mongoose.connect(db, {
 	useNewUrlParser: true,
